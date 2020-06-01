20ba468082e37590d3fc356abb61c5f36bf7d0ee
STRIX247 <wesleyversion2@gmail.com>
2020-06-02 04:08:38 +0530

    Let's Pixy-ify
    > Device bringup for pixy OS

--- pixys_ginkgo.mk
+++ pixys_ginkgo.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 The Pixys Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,14 +13,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# ANXCamera
-$(call inherit-product, vendor/ANXCamera/config.mk)
+# Inherit some common Pixys stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_ginkgo
+PRODUCT_NAME := pixys_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
